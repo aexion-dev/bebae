5530d40b3c314e868baac1c26728c4dbfc79b376
Aexi0n <alexandher.me@gmail.com>
2020-05-15 11:51:16 -0500

    Added Homepage Component and installed node-sass

---
+++ src/homepage.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const HomePage = () => (
+  <div className='homepage'>
+    <div className='directory-menu'>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>HATS</h1>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>HOODIES</h1>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>SHORTS</h1>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>T-SHIRTS</h1>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>SHOES</h1>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+export default HomePage;
