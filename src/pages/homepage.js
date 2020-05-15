f7f4d4368fb65d6e1d1b65a3a3c2c449904bdb89
Aexi0n <alexandher.me@gmail.com>
2020-05-15 15:01:03 -0500

    added directory, menu items and styling

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,41 +1,9 @@
 import React from 'react';
+import Directory from '../components/directory/directory';
 import './homepage.scss'
 
 const HomePage = () => (
-  <div className='homepage'>
-    <div className='directory-menu'>
-      <div className='menu-item'>
-        <div className='content'>
-          <h1 className='title'>HATS</h1>
-          <span className='subtitle'>SHOP NOW</span>
-        </div>
-      </div>
-      <div className='menu-item'>
-        <div className='content'>
-          <h1 className='title'>HOODIES</h1>
-          <span className='subtitle'>SHOP NOW</span>
-        </div>
-      </div>
-      <div className='menu-item'>
-        <div className='content'>
-          <h1 className='title'>SHORTS</h1>
-          <span className='subtitle'>SHOP NOW</span>
-        </div>
-      </div>
-      <div className='menu-item'>
-        <div className='content'>
-          <h1 className='title'>T-SHIRTS</h1>
-          <span className='subtitle'>SHOP NOW</span>
-        </div>
-      </div>
-      <div className='menu-item'>
-        <div className='content'>
-          <h1 className='title'>SHOES</h1>
-          <span className='subtitle'>SHOP NOW</span>
-        </div>
-      </div>
-    </div>
-  </div>
+  <Directory />
 )
 
 export default HomePage;
