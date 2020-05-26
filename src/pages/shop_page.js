64553f9455b9fdb9e8c9f76e11ee8e351d338443
Aexi0n <alexandher.me@gmail.com>
2020-05-26 18:03:17 -0500

    moved directory/shop data into reducer, created selectors for ux flow

--- src/pages/shop_page.js
+++ src/pages/shop_page.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import CollectionsOverview from '../components/collections-overview/collections-overview';
+import CollectionPage from './collection_page';
 import './shop_page.scss';
 
 
-const ShopPage = () => (
+const ShopPage = ({ match }) => (
   <div className='shop-page'>
-    <CollectionsOverview />
+    <Route exact path={`${match.path}`} component={CollectionsOverview}/>
+    <Route exact path={`${match.path}/:collectionId`} component={CollectionPage} />
   </div>
 );
 
