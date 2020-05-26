a907a311ca7909f56171826b6116b7850344e7ec
Aexi0n <alexandher.me@gmail.com>
2020-05-26 17:10:53 -0500

    moved shop/directory data to redux reducers

--- src/pages/shop_page.js
+++ src/pages/shop_page.js
@@ -1,31 +1,12 @@
 import React from 'react';
+import CollectionsOverview from '../components/collections-overview/collections-overview';
 import './shop_page.scss';
-import CollectionPreview from '../components/collection-preview/collection-preview';
-
-import SHOP_DATA from '../data/shop_data';
 
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections: SHOP_DATA
-    }
-  }
-
-  render() {
-    const {collections} = this.state;
-    return (
-      <div className='shop-page'>
-        {
-        collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))
-        }
-      </div>
-    )
-  }
-}
+const ShopPage = () => (
+  <div className='shop-page'>
+    <CollectionsOverview />
+  </div>
+);
 
 export default ShopPage;
