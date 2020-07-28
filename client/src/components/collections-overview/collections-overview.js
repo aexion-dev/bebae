6d1e91700b910ed124cf21903b9b3d73f2c44418
Aexi0n <alexandher.me@gmail.com>
2020-07-28 17:46:31 -0500

    added collection button link to collection page

--- client/src/components/collections-overview/collections-overview.js
+++ client/src/components/collections-overview/collections-overview.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
@@ -6,7 +7,7 @@ import collectionImage from '../../assets/img/collection-image-sample.png';
 import CustomButton from '../custom-button/custom-button';
 import './collections-overview.scss';
 
-const CollectionsOverview = ({ collections }) => {
+const CollectionsOverview = ({ collections, history, match }) => {
   const [activeId, setActiveId] = useState(collections[0].id);
 
   const handleClick = (id) => {
@@ -18,20 +19,22 @@ const CollectionsOverview = ({ collections }) => {
       {
         collections
           .filter(collection => collection.id === activeId)
-          .map(({ id, name, imageUrl }) => (
+          .map(({ id, name, imageUrl, routeName }) => (
             <div key={id} className="collection-details">
               <div className="image-container">
                 <img className="collection-image" src={collectionImage} alt="" />
-                <img className="collection-icon" src="#" alt="" />
+                <img className="collection-icon" src="" alt="" />
               </div>
-              <h2 className="collection-title">Garden Collection</h2>
+              <h2 className="collection-title">{name}</h2>
               <p className="collection-desc">Lorem ipsum dolor sit amet,
                 consectetur adipiscing elit. Sed eu tellus ex. Etiam vel
                 tincidunt libero. Phasellus vel faucibus arcu, vitae liquam leo.
                 Nunc urna quam, ultrices vitae arcu sit amet, egestas tincidunt
                 nisl. Vivamus neque justo, gravida sit amet arcu non, vehicula
                 fringilla ipsum. Proin eget tincidunt ante, ac mollis tellus.</p>
-              <CustomButton className="collection-btn" type='submit'>Shop Collection</CustomButton>
+              <CustomButton
+                className="collection-btn"
+                onClick={() => history.push(`${match.url}/${routeName}`)}>Shop Collection</CustomButton>
             </div>
         ))
       }
@@ -56,4 +59,4 @@ const mapStateToProps = createStructuredSelector({
   collections: selectCollectionsForPreview
 })
 
-export default connect(mapStateToProps)(CollectionsOverview);
+export default withRouter(connect(mapStateToProps)(CollectionsOverview));
